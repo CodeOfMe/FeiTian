65614f75b94fa4bf42600fd220fd26a63a2c21ba
Akino <6130092+cycleuser@users.noreply.github.com>
2026-07-20 06:21:26 +0800

    chore: cleanup temp files

--- FeiTian.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-C:\Miniconda3\envs\dev\python.exe -m feitian > "C:\Users\frede\Documents\GitHub\CodeOfMe\FlyingDream\feitian_error.log" 2>&1
--- run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-call C:\Miniconda3\Scripts\activate.bat dev
-cd /d C:\Users\frede\Documents\GitHub\CodeOfMe\FlyingDream
-pip install -e . -q
-python -m feitian
-pause
